ad6afe033a98de127f6ea3d6d70312c6fdc58187
Burak GIZLICE <87895075+Downshifter114@users.noreply.github.com>
2022-04-15 01:01:47 +0300

    Console App Heart

--- Crypto Monitor/Crypto Monitor/Program.cs
+++ Crypto Monitor/Crypto Monitor/Program.cs
@@ -11,135 +11,83 @@ namespace Crypto_Monitor
 {
     class Program
     {
-        //Global variables.
-        public static string command;
-        public static string orderbyInput;
-        public static int pageInput = 1;
-        public static Timer newTimer = new Timer();
+        static Timer timer;
+        static string url;
+        //User Input variables.
+        static string userInput;
+        static int pageInput;
+        static string orderInput;
         static void Main(string[] args)
         {
+            WakeUp();    
+            while (true)
+            {
+                if (userInput == "clear") Console.Clear();
+                if (userInput == "help") Help();
+                if (userInput == "end") Stop();
+                if (userInput == "quit") System.Environment.Exit(0);
+                if (userInput == "start") Start(); //Start the timer.
+                GetInput();
+            }
+        }      
+        
+        //Fundamental Methods.
+        private static void WakeUp()
+        {
+            int seconds = 240;
+            //Setting up the timer
+            timer = new System.Timers.Timer();
+            timer.Interval = seconds * 1000;
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            //WakeUp
             Help();
-            GetInput();
-            
-
-            Console.ReadKey();
-        }        
-
-        public static void Help()
-        {
-            Write("WELCOME!", ConsoleColor.DarkYellow);
-            Write("\n-The format to start monitoring is:", ConsoleColor.DarkYellow); Write(" 'start, page x, orderby x'", ConsoleColor.DarkRed);
-            Write("\n-Page options are:", ConsoleColor.DarkYellow); Write(" numbers. (0, 1, 2, ...)", ConsoleColor.DarkRed);
-            Write("\n-Orderby options are:", ConsoleColor.DarkYellow); Write(" ('price', '24hrs', '7days')", ConsoleColor.DarkRed);
-            Write("\n-To end the repeating process type", ConsoleColor.DarkYellow); Write(" 'end'", ConsoleColor.DarkRed);
-            Write("\n-To close the app, type", ConsoleColor.DarkYellow); Write(" 'quit'", ConsoleColor.DarkRed);
-            Write("\n-To see this message again, type", ConsoleColor.DarkYellow); Write(" 'help'", ConsoleColor.DarkRed);
-            Write("\nEXAMPLE: ", ConsoleColor.DarkYellow); Write("start, page 1, orderby rank", ConsoleColor.DarkRed);
+            GetInput();            
         }
-
-        //Write("\nInvalid input. Try again! EXAMPLE: ", ConsoleColor.DarkYellow); Write("start, page 1, orderby rank", ConsoleColor.DarkRed);
-        public static void GetInput()
+        static void GetInput()
         {
-            Console.Write("\nYour Input:");
-            string userInput = Console.ReadLine();
-            List<string> list = userInput.Split(", ").ToList();
-            if (list.Count == 1)
+            Write("\nYour Input:", ConsoleColor.DarkYellow);
+            userInput = Console.ReadLine();
+            //Assigning userInput variables.
+            if (userInput != "clear" && userInput != "end" && userInput != "quit" && userInput != "help" && !isValid(userInput)) 
+                userInput = "nonesense";
+            else if (isValid(userInput))
             {
-                if (list[0] == "quit")
-                    System.Environment.Exit(0);
-                else if (list[0] == "end")
-                {
-                    newTimer.Stop();
-                    GetInput();
-                }
-                else
-                    Invalid();
-
+                List<string> primaryList = userInput.Split(", ").ToList();
+                List<string> secondaryList = primaryList[1].Split(' ').ToList();
+                pageInput = int.Parse(secondaryList[1]);
+                secondaryList = primaryList[2].Split(' ').ToList();
+                orderInput = secondaryList[1];
+                userInput = "start";
             }
-            else if (list.Count == 3)
-            {
-                if (list[0] == "start" && list[1].Contains("page ") && list[2].Contains("orderby "))
-                {
-                    //Double checking
-                    List<string> temporaryPage = list[1].Split(' ').ToList();
-                    if (temporaryPage.Count != 2 || !IsNumeric(temporaryPage[1]) || temporaryPage[1].Count() > 2) 
-                        Invalid();
-                    List<string> temporaryOrder = list[2].Split(' ').ToList(); 
-                    if (temporaryOrder.Count != 2 || temporaryOrder[1] != "price" && temporaryOrder[1] != "24hrs" && temporaryOrder[1] != "7days" && temporaryOrder[1] != "rank") 
-                        Invalid();
-
-                    command = list[0];
-                    //pageinput
-                    orderbyInput = temporaryPage[1];
-
-                    if (command == "start")
-                        StartTimer(10);
-
-                    GetInput();
-                }
-                else
-                    Invalid();
-            }
-            else            
-                Invalid();
-            
+            //Generating url according to page number.
+            url = "https://api.coinlore.net/api/tickers/?start=" + ((pageInput - 1) * 50).ToString() + "&limit=50";
         }
-
-        public static void Invalid()
+        static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
         {
-            Write("\nInvalid input. Try again! EXAMPLE: ", ConsoleColor.DarkYellow); Write("start, page 1, orderby rank", ConsoleColor.DarkRed);
-            GetInput();
-        }
-
-
-        public static void StartTimer(int seconds)
-        {
-            int startPoint = (pageInput - 1) * 100;
-            string url = "https://api.coinlore.net/api/tickers/?start=" + startPoint + "&limit=100";           
-            //Timer Added Versions            
-            newTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            newTimer.Interval = seconds * 1000;
-            newTimer.Start();
-        }
-
-        private static void OnTimedEvent(object source, ElapsedEventArgs e)
-        {
-            int startPoint = (pageInput - 1) * 100;
-            string url = "https://api.coinlore.net/api/tickers/?start=" + startPoint + "&limit=100";
-            CoinList coinList = getMarketInfo(url, orderbyInput);
+            Console.Clear();
+            CoinList coinList = getMarketInfo(url, orderInput);
             WriteTable(coinList);
         }
-
-        public static CoinList getMarketInfo(string url, string orderby)
+        static void TableInAdvance()
         {
-            var _url = url;
-            string JSONstring;
-            WebRequest request = WebRequest.Create(_url);
-            WebResponse response = request.GetResponse();
-            Console.WriteLine("\n\nServer Connection Status: " + ((HttpWebResponse)response).StatusDescription);
-            using (Stream dataStream = response.GetResponseStream())
-            {
-                StreamReader reader = new StreamReader(dataStream);
-                JSONstring = reader.ReadToEnd();
-            }
-            CoinList coinList = JsonConvert.DeserializeObject<CoinList>(JSONstring);
-            CoinList sortedPrice = new CoinList();
-            response.Close();
-
-            //Order
-            if (orderby == "price")
-                sortedPrice.data = coinList.data.OrderByDescending(x => x.price_usd).ToList();
-            if (orderby == "24hrs")
-                sortedPrice.data = coinList.data.OrderByDescending(x => x.percent_change_24h).ToList();
-            if (orderby == "7days")
-                sortedPrice.data = coinList.data.OrderByDescending(x => x.percent_change_7d).ToList();
-            if (orderby == null || orderby == "rank")
-                sortedPrice = coinList;
-
-            return sortedPrice;
+            Console.Clear();
+            CoinList coinList = getMarketInfo(url, orderInput);
+            WriteTable(coinList);
         }
-
-        public static void WriteTable(CoinList list)
+        static void Start()
+        {
+            Write("\nBringing the results...\n", ConsoleColor.Cyan);
+            TableInAdvance();
+            timer.Enabled = true;
+        }
+        static void Stop()
+        {            
+            timer.Enabled = false;
+            Console.Clear();
+            Write("Process ended. To see the commands, type", ConsoleColor.DarkYellow); Write(" 'help'\n", ConsoleColor.DarkRed);
+        }
+        static void WriteTable(CoinList list)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("-------------------------------------------------------------------------------");
@@ -171,20 +119,74 @@ namespace Crypto_Monitor
             }
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("-------------------------------------------------------------------------------\n\n");
-        }
+        }                
+        static CoinList getMarketInfo(string url, string orderby)
+        {
+            var _url = url;
+            string JSONstring;
+            WebRequest request = WebRequest.Create(_url);
+            WebResponse response = request.GetResponse();
+            Write("\n\nServer Connection Status: " + ((HttpWebResponse)response).StatusDescription + "\n", ConsoleColor.Cyan);
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(dataStream);
+                JSONstring = reader.ReadToEnd();
+            }
+            CoinList coinList = JsonConvert.DeserializeObject<CoinList>(JSONstring);
+            CoinList sortedPrice = new CoinList();
+            response.Close();
 
-        public static void Write(string writeText, ConsoleColor color)
+            //Order
+            if (orderby == "price")
+                sortedPrice.data = coinList.data.OrderByDescending(x => x.price_usd).ToList();
+            if (orderby == "24hrs")
+                sortedPrice.data = coinList.data.OrderByDescending(x => x.percent_change_24h).ToList();
+            if (orderby == "7days")
+                sortedPrice.data = coinList.data.OrderByDescending(x => x.percent_change_7d).ToList();
+            if (orderby == null || orderby == "rank")
+                sortedPrice = coinList;
+
+            return sortedPrice;
+        }
+        static bool isValid(string input)
+        {
+            //General Check (checks 'start' spesifically).
+            List<string> primaryList = input.Split(", ").ToList();
+            if (primaryList.Count != 3) return false;
+            if (primaryList[0] != "start") return false;
+            if (!primaryList[1].Contains("page ")) return false;
+            if (!primaryList[2].Contains("orderby ")) return false;
+            //Spesific check for page input.
+            List<string> secondaryList = primaryList[1].Split(' ').ToList();
+            if (secondaryList.Count != 2) return false;
+            if (!int.TryParse(secondaryList[1], out int ignoreMe)) return false;
+            //Spesific check for orderby input.
+            secondaryList = primaryList[2].Split(' ').ToList();
+            if (secondaryList.Count != 2) return false;
+            if (secondaryList[1] != "rank" && secondaryList[1] != "24hrs" && secondaryList[1] != "7days" && secondaryList[1] != "price") return false;
+            //If passed all tests, return true.
+            return true;
+        }
+        
+
+        //Helper Methods.
+        static void Help()
+        {
+            Write("\nWELCOME!", ConsoleColor.DarkYellow);
+            Write("\n-The format to start monitoring is:", ConsoleColor.DarkYellow); Write(" 'start, page x, orderby x'", ConsoleColor.DarkRed);
+            Write("\n-Page options are:", ConsoleColor.DarkYellow); Write(" numbers. (0, 1, 2, ...)", ConsoleColor.DarkRed);
+            Write("\n-Orderby options are:", ConsoleColor.DarkYellow); Write(" ('price', '24hrs', '7days')", ConsoleColor.DarkRed);
+            Write("\n-To end the repeating process type", ConsoleColor.DarkYellow); Write(" 'end'", ConsoleColor.DarkRed);
+            Write("\n-To close the app, type", ConsoleColor.DarkYellow); Write(" 'quit'", ConsoleColor.DarkRed);
+            Write("\n-To see this message again, type", ConsoleColor.DarkYellow); Write(" 'help'", ConsoleColor.DarkRed);
+            Write("\n-To clean the console, type", ConsoleColor.DarkYellow); Write(" 'clear'", ConsoleColor.DarkRed);
+            Write("\nEXAMPLE: ", ConsoleColor.DarkYellow); Write("start, page 1, orderby rank\n", ConsoleColor.DarkRed);
+        }
+        static void Write(string writeText, ConsoleColor color)
         {
             Console.ForegroundColor = color;
             Console.Write(writeText);
             Console.ResetColor();
-        }
-
-        public static bool IsNumeric(string value)
-        {
-            return value.All(char.IsNumber);
-        }
+        }        
     }
-
-
 }
